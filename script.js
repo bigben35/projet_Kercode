b2fab80645c52fd92b2c6bb4b0a29571c9374980
Josselin Crenn <josselincrenn@gmail.com>
2022-01-23 22:52:18 +0100

    modif inchangées script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-// let adresse = document.getElementById('address');
-// console.log(adresse.value);
+let adresse = document.getElementById('address');
+console.log(adresse.value);
 
-// fetch ("https://api-adresse.data.gouv.fr/search/?q="+adresse.value+"&limit=6&autocomplete=1")
-// .then(response => console.log(response))
-// .then(data => {
-//     let adresses = data.features;
-//     console.log(data);
 
-//     adresse.addEventListener('input', displayAddress)
 
-//     function displayAddress() {
+fetch ("https://api-adresse.data.gouv.fr/search/?q="+adresse.value+"&limit=6&autocomplete=1")
+.then(response => console.log(response))
+.then(data => {
+    let adresses = data.features;
+    console.log(data);
+
+    adresse.addEventListener('input', displayAddress)
+
+    function displayAddress() {
     
     
-//     }
-// })
+    }
+})
 
 
 
