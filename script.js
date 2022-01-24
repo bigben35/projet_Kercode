d30ab87c6e1de8700970fd131939d51d82b216d4
bigben35 <79405849+bigben35@users.noreply.github.com>
2022-01-24 16:27:13 +0100

    changement de class dans le menu du header et tentative class active pour le menu du header dans script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// FORMULAIRE DE CONTACT / RECUPERER ADRESSE AVEC API.GOUV
+
+
 let adresse = document.getElementById('address');
 let listeAdresse = document.getElementById('listAddress');
 console.log(adresse.value);
@@ -40,14 +43,27 @@ function displayAddress() {
 
 
 
+// MANIPULATION DU DOM
+
 
 let button = document.querySelector('.send-msg');
 // console.log(button);
-checkbox = document.getElementById('autorisation');
+let checkbox = document.getElementById('autorisation');
 // console.log(checkbox);
 
+
+// button.classList.add('noHover');
+// button.addEventListener('mouseover', () => {
+    
+//         button.style.transform = scale(1.1);
+//         button.style.backGroundColor = "orange";
+
+// }) 
+
+
 checkbox.addEventListener('click', () => {
-    button.classList.toggle('active');
+    button.classList.toggle('activeBtn');
+    
     // button.classList.remove('send-msg:hover');
 })
 
@@ -59,9 +75,28 @@ checkbox.addEventListener('click', () => {
 //     }, 4000);
 //   });
 
-if(checkbox === true){
+if(checkbox){
     button.addEventListener('click', () => {
-    alert('Message');
+    alert('Votre message a bien été envoyé !');
 })
 }
  
+
+
+// MENU CLASS ACTIVE POUR LA LISTE ACTIVE
+
+
+// Get the container element
+let liContainer = document.querySelector('.menu_box');
+
+// Get all buttons with class="btn" inside the container
+let lis = liContainer.getElementsByClassName("menu_item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < lis.length; i++) {
+  lis[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
