ede3a6b3ab61c28158640588d87653aec5faa7f7
bigben35 <79405849+bigben35@users.noreply.github.com>
2022-01-25 14:02:30 +0100

    creation asideConsei.php et implémentation dans les pages produits

--- page_cantillon.php
+++ page_cantillon.php
@@ -51,47 +51,9 @@
 
 
 
-    <aside id="infos-conseils" class="container">
-        <div class="bloc-conseil">
-            <img src="images/grape.webp" alt="conseil en accords mets et vins">
-            <div class="orange-line-title">
-                <h4>Nos Conseils</h4>
-                <span class="orange-line"></span>
-            </div>
-            <p>Au <strong>02 41 43 70 70</strong> ou par email <a href="contact_Ty-Joss.html">en cliquant ici</a></p>
-        </div>
-        <div class="bloc-conseil">
-            <img src="images/world.webp" alt="sauver les vignerons">
-            <div class="orange-line-title">
-                <h4>Soutenons Nos Producteurs</h4>
-                <span class="orange-line"></span>
-            </div>
-            <p>"Boire un canon, c'est sauver un vigneron"</p>
-        </div>
-        <div class="bloc-conseil">
-            <img src="images/clicandcollect.webp"
-                alt="click and collect, choisissez votre vin et venez le récupérer chez Ty Joss">
-            <div class="orange-line-title">
-                <h4>Click & Collect</h4>
-                <span class="orange-line"></span>
-            </div>
-            <p>Validez votre panier et retirez votre commande à la cave <br><br><i
-                    class="fas fa-lock"></i>&nbsp;<strong>Paiement sécurisé</strong></p>
-        </div>
-    </aside>
-
-    <aside id="newsletter">
-        <div>
-            <h3>Inscrivez-vous à notre Newsletter</h3>
-            <p class="sous-titre">Et recevez toute notre actualité</p>
-        </div>
-        <div class="input-newsletter">
-            <input type="email" name="email" placeholder="Entrez votre adresse mail ici...">
-        </div>
-        <div class="btn-newsletter">
-            <button type="submit">Envoyer</button>
-        </div>
-    </aside>
+    <?php
+        include "commun/asideConseil.php";
+   ?>
 
     <?php
         include "commun/footer.php";
